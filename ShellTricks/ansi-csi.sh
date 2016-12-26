bbe63f9a8ff38c49101be988e239b9d03a521c17
maxwell92 <710028463@qq.com>
2016-12-26 14:06:40 +0800

    add asci csi

---
+++ ShellTricks/ansi-csi.sh
@@ -0,0 +1,3 @@
+echo -e "\033[1;31m I Love You \e[0m"
+
+http://mp.weixin.qq.com/s?__biz=MzAxMzc4Mzk1Mw==&mid=2649836777&idx=1&sn=a684326b9fb5d8f8db719751cb39fb4c&chksm=8398aa17b4ef23010fc2050f82bc3dc9c300f05f7fb4dbc9773c2117ec51f3e41b70cb9863a0&scene=0#rd
