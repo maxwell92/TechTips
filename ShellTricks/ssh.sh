aedd2cb7d358a2e6048f642bff5e420ca7c61f6b
maxwell92 <710028463@qq.com>
2017-01-14 17:51:56 +0800

    add ssh public key

--- ShellTricks/ssh.sh
+++ ShellTricks/ssh.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
-sudo apt-get install openssh-server
+sudo apt-get install openssh-server:
 
 http://www.cnblogs.com/xiazh/archive/2010/08/13/1798844.html
+
+solving dpkg locked problem:
+http://blog.csdn.net/kevin_android_123456/article/details/8174343
+
+ssh public key:
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+https://coding.net/help/doc/git/ssh-key.html
+
+
