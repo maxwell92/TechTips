ac15755525f955cd0696024afc9cf571ebe0cab0
maxwell92 <710028463@qq.com>
2017-01-16 17:57:50 +0800

    add go simple web server

--- OFS/a-brief-introduction-to-golang/demo/web/main.go
+++ OFS/a-brief-introduction-to-golang/demo/web/main.go
@@ -6,5 +6,9 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from %s\n", r.URL.Path)
+	})
 
+	http.ListenAndServe(":8181", nil)
 }
